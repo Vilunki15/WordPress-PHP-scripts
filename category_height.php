b22cb8d95095be94f5d57558b1cd351314301f2c
Vilunki <86688634+Vilunki15@users.noreply.github.com>
2025-07-31 21:03:53 +0300

    Create category_height.php

---
+++ category_height.php
@@ -0,0 +1,13 @@
+//When you have a lot of WooC categories, the category metabox height is way too small. 
+//This modifies height to see more categories once!
+function metabox_height() {
+	echo '
+	<style>
+    #product_catdiv .tabs-panel {
+        max-height: 1000px !important;
+        overflow-y: auto !important;
+    }
+    </style>
+	';
+}
+add_action('admin_head', 'metabox_height');
