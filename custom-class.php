ac97d7505493f4ec8c7f8301d0f3b794427734aa
Vilunki <86688634+Vilunki15@users.noreply.github.com>
2025-08-01 12:34:15 +0300

    Added highlighting

--- custom-class.php
+++ custom-class.php
@@ -1,5 +1,7 @@
+<?php
 //Sometimes you need custom classes for the body element
 //This adds custom classes for WP pages (e.g., my account page)
+//<?php is only for highlighting
 
 add_filter('body_class', function($classes) {
     if (is_account_page()) {
